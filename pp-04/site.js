9f59c6fa082385ebf90abc2e7ab49368069bcb8b
David Kotaev <davidkotaev@gmail.com>
2018-11-12 19:12:11 -0600

    Passed linting on pp04

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,27 @@
+var noScriptId = "#doorbell-nojs";
+var scriptedId = "#doorbell-js";
+var scriptedClass = "doorbell-button-js";
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+function replaceNoScript(controlId) {
+  var control = document.querySelector(controlId);
+  var parent = control.parentNode;
+  var button = document.createElement('button');
+  button.setAttribute("id", scriptedId);
+  button.setAttribute("class", scriptedClass);
+  button.textContent = "Ring!";
+  // insert scripted button
+  parent.insertBefore(button, control);
+  // register an event listener
+  button.addEventListener('click', ringDoorbell);
+  // delete old control
+  control.remove();
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  replaceNoScript(noScriptId);
+});
